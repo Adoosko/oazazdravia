1afd08477340d3abc9f87d2a7c419aa140e6c3b2
Adoosko <120666532+Adoosko@users.noreply.github.com>
2024-05-29 21:50:11 +0200

    j

--- app/page.js
+++ app/page.js
@@ -1,15 +1,5 @@
-import Content from "@/components/Content";
-import Hero from "@/components/Hero";
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <main className=" relative mx-auto text-center max-w-[1440px] items-center flex flex-col justify-center ">
-      <Hero />
-      <div>
-        <Image src={"/banner.png"} alt="banner" width={1500} height={800} />
-      </div>
-      <Content />
-    </main>
+    <main className=" relative mx-auto text-center max-w-[1440px] items-center flex flex-col justify-center "></main>
   );
 }
