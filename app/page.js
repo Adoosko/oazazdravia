49d419309b1d471b41a6b73d1426dbda37d8e219
Adoosko <120666532+Adoosko@users.noreply.github.com>
2024-05-29 21:52:30 +0200

    fixed imports

--- app/page.js
+++ app/page.js
@@ -1,5 +1,11 @@
 export default function Home() {
   return (
-    <main className=" relative mx-auto text-center max-w-[1440px] items-center flex flex-col justify-center "></main>
+    <main className=" relative mx-auto text-center max-w-[1440px] items-center flex flex-col justify-center ">
+      <Hero />
+      <div>
+        <Image src={"/banner.png"} alt="banner" width={1500} height={800} />
+      </div>
+      <Content />
+    </main>
   );
 }
